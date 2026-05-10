7ee4903c8d9e38b2d2b9b3419a930a0c6d154840
Derby1537 <dellerba.gabriele@gmail.com>
2026-05-10 17:53:34 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gabriele Dell’Erba on 10/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,18 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by Gabriele Dell’Erba on 10/05/2026.
+//
+
+import Testing
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
